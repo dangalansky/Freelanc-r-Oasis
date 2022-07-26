04907038c6d46b0df5890c5940f7fb5fca56da37
dan_hates_the_future <97214702+dangalansky@users.noreply.github.com>
2022-07-25 20:23:54 -0600

    Add files via upload

--- server.py
+++ server.py
@@ -1,81 +1,57 @@
 import os
-from flask import Flask, render_template, request, flash, redirect, url_for
-from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, URLField, SelectField
-from flask_wtf.file import FileField, FileRequired
-from wtforms.validators import DataRequired, URL
-from flask_bootstrap import Bootstrap
 import csv
-from werkzeug.utils import secure_filename
-
-
-UPLOAD_FOLDER = '/Users/dangalansky/Dropbox/pythonProject/Portfolio/Freelance-Oasis/static/images'
-ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
+import random
+from flask import Flask, render_template, redirect, url_for
+from flask_wtf import FlaskForm
+from wtforms import SubmitField, StringField, URLField, SelectField
+from flask_wtf.file import FileField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
-app.secret_key = 'gh_Bjdl4(kdasdfkJn.)mQ'
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-WTF_CSRF_SECRET_KEY = 'gh_Bjdl4(kdasdfkJn.)mQ'
-Bootstrap(app)
+app.config['SECRET_KEY'] = 'secret'
+app.config['UPLOAD_FOLDER'] = 'static/images'
+ALLOWED_EXTENSIONS = {'png'}
 
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-class AddCafe(FlaskForm):
-    cafe = StringField(label='Cafe name', validators=[DataRequired()])
-    photo = FileField(label='Photo Upload', validators=[FileRequired()])
-    location = URLField(label='Google Maps URL:', validators=[DataRequired()])
-    open = StringField(label='Opening Time e.g. 8AM', validators=[DataRequired()])
-    close = StringField(label='Closing Time e.g. 5PM', validators=[DataRequired()])
-    coffee = SelectField(label='Rate Coffee Quality', choices=['⭐', '⭐⭐', '⭐⭐⭐', '⭐⭐⭐⭐', '⭐⭐⭐⭐⭐'])
-    wifi = SelectField(label='WIFI?', choices=['YUP!', 'NOPE'])
-    submit = SubmitField('Submit')
-
-class PhotoForm(FlaskForm):
-    photo = FileField(validators=[FileRequired()])
-    submit = SubmitField('Submit')
-
-
+class AddCafeForm(FlaskForm):
+    cafe = StringField("Cafe Name", validators=[DataRequired()])
+    address = StringField("Address", validators=[DataRequired()])
+    open = StringField("Opening Time (i.e. 6AM)", validators=[DataRequired()])
+    close = StringField("Closing Time (i.e. 5PM)", validators=[DataRequired()])
+    coffee = SelectField("Rate Coffee", choices=['☕', '☕☕', '☕☕☕', '☕☕☕☕', '☕☕☕☕☕'])
+    wifi = SelectField("WIFI?", choices=['YUP', 'NOPE'])
+    photo = FileField("Upload Photo (must be .png!)", validators=[DataRequired()])
+    submit = SubmitField("Submit")
 
 @app.route('/')
 def home():
-    return render_template('index.html')
-
-@app.route('/elements', methods=['GET', 'POST'])
-def elements():
-    form = PhotoForm()
-    if form.validate_on_submit():
-        # f = form.photo.data
-        # filename = secure_filename(f.filename)
-        # f.save(os.path.join(
-        #     app.config['UPLOAD_FOLDER'], filename
-        # ))
-        return redirect(url_for('home'))
-
-    return render_template('elements.html', form=form)
-
-
-@app.route('/cafe_list')
-def cafe_list():
-    with open('data.csv', newline='') as csv_file:
-        csv_data = csv.reader(csv_file, delimiter=',')
-        list_of_rows = []
-        for row in csv_data:
-            list_of_rows.append(row)
-    return render_template('cafe_list.html', cafes=list_of_rows)
-
+    with open('data.csv', newline='') as file:
+        data = csv.reader(file, delimiter=',')
+        list = []
+        for row in data:
+            list.append(row)
+    return render_template('index.html', cafes=list)
 
 @app.route('/add_cafe', methods=['GET', 'POST'])
 def add_cafe():
-    form = AddCafe()
+    form = AddCafeForm()
     if form.validate_on_submit():
+        num = random.randint(1, 6)
         with open('data.csv', 'a') as file:
-            file.write(
-                f'\n{form.cafe.data},{form.photo.data},{form.location.data},{form.open.data},{form.close.data},{form.coffee.data},{form.wifi.data}')
-        return cafe_list()
-    return render_template('add_cafe.html', form=form)
-
+            file.write(f'\n{form.cafe.data},{form.address.data},{form.open.data},{form.close.data},{form.coffee.data},{form.wifi.data},{num}')
+        file = form.photo.data
+        if file and allowed_file(file.filename):
+            ext = file.filename.split(".")[1]
+            full_filename = (os.path.join(
+                app.config['UPLOAD_FOLDER'], f'{form.cafe.data}.{ext}'))
+            file.save(full_filename)
+            return home()
+        else:
+            return "Unsupported File Type"
+    return render_template("add_cafe.html", form=form)
 
 if __name__ == '__main__':
     app.run(port=5001, debug=True)
